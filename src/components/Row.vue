9a4c2dae008dd16b3a96a97fa38de8b828d257ef
@laura.alves <laura.alves@gocase.com.br>
2023-08-21 16:39:12 -0300

    Fix: new headers

--- src/components/Row.vue
+++ src/components/Row.vue
@@ -3,10 +3,27 @@
     <Cell :content="index" />
     <Cell :content="rowContent.moqaID" />
     <Cell :content="formatedDate" />
+    <Cell :content="rowContent.extTemp"/>
+    <Cell :content="rowContent.alt" />
+    <Cell :content="rowContent.rtcLog" />
+    <Cell :content="rowContent.codeID" />
+    <Cell :content="rowContent.boardID" />
     <Cell :content="rowContent.Pres" />
-    <Cell :content="rowContent.co2" />
+    <Cell :content="rowContent.hum" />
+    <Cell :content="rowContent.intTemp" />
+    <Cell :content="rowContent.pm1" />
     <Cell :content="rowContent.pm25" />
-    <Cell :content="rowContent.pm10" />
+    <Cell :content="rowContent.adc0" />
+    <Cell :content="rowContent.co2" />
+    <Cell :content="rowContent.adc1" />
+    <Cell :content="rowContent.adc2" />
+    <Cell :content="rowContent.adc3" />
+    <Cell :content="rowContent.tvocs" />
+    <Cell :content="rowContent.adsLog" />
+    <Cell :content="rowContent.ccsLog" />
+    <Cell :content="rowContent.bmeLog" />
+    <Cell :content="rowContent.pmsLog" />
+    <Cell :content="rowContent.msdLog" />
   </tr>
 </template>
 
@@ -24,7 +41,7 @@ export default {
   },
   computed: {
     formatedDate () {
-        return new Date(this.rowContent.Timestamp.seconds * 1000).toISOString()
+        return new Date(this.rowContent?.Timestamp?.seconds * 1000).toISOString()
     }
   }
 }
--- src/components/TableHeader.vue
+++ src/components/TableHeader.vue
@@ -4,10 +4,27 @@
       <HeaderCell label="Nº" />
       <HeaderCell label="moqaID" />
       <HeaderCell label="Timestamp" />
-      <HeaderCell label="Pressure" />
-      <HeaderCell label="co2" />
+      <HeaderCell label="extTemp" />
+      <HeaderCell label="alt" />
+      <HeaderCell label="codeID" />
+      <HeaderCell label="boardID" />
+      <HeaderCell label="Pres" />
+      <HeaderCell label="hum" />
+      <HeaderCell label="intTemp" />
+      <HeaderCell label="pm1" />
       <HeaderCell label="pm25" />
-      <HeaderCell label="pm10" />
+      <HeaderCell label="adc0" />
+      <HeaderCell label="co2" />
+      <HeaderCell label="adc1" />
+      <HeaderCell label="adc2" />
+      <HeaderCell label="adc3" />
+      <HeaderCell label="tvocs" />
+      <HeaderCell label="adsLog" />
+      <HeaderCell label="ccsLog" />
+      <HeaderCell label="bmeLog" />
+      <HeaderCell label="pmsLog" />
+      <HeaderCell label="msdLog" />
+      <HeaderCell label="rtcLog" />
     </tr>
   </thead>
 </template>
