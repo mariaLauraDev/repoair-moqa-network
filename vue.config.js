63aaadf421e3908a1d3c2250d2b203a3637a79f4
@laura.alves <laura.alves@gocase.com.br>
2023-08-26 22:05:00 -0300

    Feat: add csp plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
+const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -8,5 +9,33 @@ module.exports = defineConfig({
         additionalData: `@import "@/assets/styles/variables.scss";`
       }
     }
-  }
+  },
+  configureWebpack: {
+    plugins: [
+      new CspHtmlWebpackPlugin(
+        {
+          'default-src': "'self'",
+          'script-src': ["'self'", "'unsafe-eval'", "https://*.firebase.com", "https://firebaseio.com"],
+          'style-src': ["'self'", "'unsafe-inline'"],
+          'img-src': ["'self'", "data:"],
+          'font-src': ["'self'", "https:"],
+          'connect-src': ["'self'", "https:"],
+          'frame-src': ["'self'"],
+          'media-src': ["'self'"],
+        },
+        {
+          enabled: true,
+          hashingMethod: 'sha256',
+          hashEnabled: {
+            'script-src': true,
+            'style-src': true,
+          },
+          nonceEnabled: {
+            'script-src': true,
+            'style-src': true,
+          },
+        }
+      ),
+    ],
+  },
 });
